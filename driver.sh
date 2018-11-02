c83ea0da13025992f585b6a724965117e4f88682
Sam Crang <sam.crang@gmail.com>
2018-11-02 13:36:41 +0000

    Remove unused helper file

--- driver.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export AWS_REGION=eu-west-2
-export AWS_ACCOUNT_NAME=run-sandbox
-export ZONE_ID=Z23SW7QP3LD4TS
-export ZONE_NAME=run-sandbox.aws.ext.govsvc.uk
-export CLUSTER_NAME=rafalp
-export MAIN_PASSWORD=password
