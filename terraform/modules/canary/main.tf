a5e109170abe657783a2a45846d044a77d867bf6
Daniel Blair <daniel.blair@digital.cabinet-office.gov.uk>
2018-12-06 11:26:15 +0000

    Refactory canary module to use github-flux module.
    Instead of duplicating the templates, use the existing github-flux module.

--- terraform/modules/canary/main.tf
+++ terraform/modules/canary/main.tf
@@ -1,26 +1,5 @@
 resource "aws_codecommit_repository" "canary" {
   repository_name = "gsp-canary-chart-${var.cluster_id}"
-}
-
-resource "local_file" "namespace" {
-  filename = "${var.addons_dir}/gsp-canary-namespace.yaml"
-  content  = "${file("${path.module}/data/namespace.yaml")}"
-}
-
-data "template_file" "helm-release" {
-  template = "${file("${path.module}/data/helm-release.yaml")}"
-
-  vars {
-    namespace  = "gsp-canary"
-    chart_git  = "${aws_codecommit_repository.canary.clone_url_http}"
-    chart_ref  = "master"
-    chart_path = "charts/gsp-canary"
-  }
-}
-
-resource "local_file" "helm-release-yaml" {
-  filename = "${var.addons_dir}/gsp-canary-helm-release.yaml"
-  content  = "${data.template_file.helm-release.rendered}"
 
   provisioner "local-exec" {
     command = "../../../scripts/initialise_canary_helm_codecommit.sh"
@@ -30,4 +9,13 @@ resource "local_file" "helm-release-yaml" {
       CODECOMMIT_REPO_URL = "${aws_codecommit_repository.canary.clone_url_http}"
     }
   }
+}
+
+module "gsp-canary-release" {
+  source = "../github-flux"
+
+  namespace  = "gsp-canary"
+  chart_git  = "${aws_codecommit_repository.canary.clone_url_http}"
+  chart_ref  = "master"
+  chart_path = "charts/gsp-canary"
 }
