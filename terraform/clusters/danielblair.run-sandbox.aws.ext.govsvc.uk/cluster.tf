30eb1870330175cdd578d6f2e941842f401a2c93
Daniel Blair <daniel.blair@digital.cabinet-office.gov.uk>
2018-12-28 16:41:59 +0000

    Testing the new ignition based k8s cluster.

--- terraform/clusters/danielblair.run-sandbox.aws.ext.govsvc.uk/cluster.tf
+++ terraform/clusters/danielblair.run-sandbox.aws.ext.govsvc.uk/cluster.tf
@@ -14,7 +14,6 @@ module "gsp-cluster" {
     cluster_id = "danielblair.run-sandbox.aws.ext.govsvc.uk"
     dns_zone_id = "Z23SW7QP3LD4TS"
     dns_zone = "run-sandbox.aws.ext.govsvc.uk"
-    ssh_authorized_keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDhje7LMEsP8/2HFBQHvGbwAVRzUuNuVZJhVjHRoZxvARg1nn9D1bWx0FnK9aWL7cmu89P8pQiWGBEjWWZzHjZWnFJ4d5hu+oJliRySbhskiuVCSgP3Z0Grx/Q4cJu35O1LkwcVi8ZbYk/Ud+5pHa/U6i8bYcvxSVb2bEY9Q2t8dF9ev1aSRNNaOj7Y+2aenAwPo9Whr4DryVrAjI/CC7phv6SOSJ4o9y5Ro3Z9yEfy42wOOEL2+Q/6j9dWMVGyuaZkJ9eXRAeRHkcpuUs/kDR07bMBUolz9tfaMZWNtGgeEgm/r39fnvs1B08Pv0sHeYxh1QrNhzG076lhSVuTzxzPYplehgZNeOVseotQHRufsVMXH5paAIMgkXISUHF1dnX4U/7biSldiCG1JNvFCcTTLTEtsPC0H8Hxt5PEr8vVe4Tdfk6KVjZOVdi1alOiDyw4O8jdNKrM8gDE83tG++/RRptxu4pBvdX65AnQeocsA8S4l9hLdB76duQYAjF6iV37rheioOj9jPJR4wi8zajeNnyDbxC5B12I7Pnqz6DQvQkvADK6x27dA6mAVIqq6ORXCXENjh8W/n+9Math2l07csnjzi67np/8a9Gn01LT9xr/xFZgY+ztnHRtDSnxXfKEx3IpOvgfmP6qan3nAIMVmfkcSWd9tnC1m+qQDy/nNQ== daniel.blair@digital.cabinet-office.gov.uk"]
     user_data_bucket_name = "${var.user_data_bucket_name}"
     user_data_bucket_region = "${var.user_data_bucket_region}"
     k8s_tag = "${var.k8s_tag}"
@@ -26,7 +25,7 @@ module "gsp-base-release" {
 
   namespace      = "gsp-base"
   chart_git      = "https://github.com/alphagov/gsp-base.git"
-  chart_ref      = "master"
+  chart_ref      = "ignition"
   chart_path     = "charts/base"
   cluster_name   = "${var.cluster_name}"
   cluster_domain = "danielblair.run-sandbox.aws.ext.govsvc.uk"
--- terraform/clusters/samcrang.run-sandbox.aws.ext.govsvc.uk/cluster.tf
+++ terraform/clusters/samcrang.run-sandbox.aws.ext.govsvc.uk/cluster.tf
@@ -14,7 +14,6 @@ module "gsp-cluster" {
     cluster_id = "samcrang.run-sandbox.aws.ext.govsvc.uk"
     dns_zone_id = "Z23SW7QP3LD4TS"
     dns_zone = "run-sandbox.aws.ext.govsvc.uk"
-    ssh_authorized_keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDHtO59/mISjM3EzlKPn3A38IdiJfhGekrR+RLMc9/+AoTFGjmIZRKNDHRHhD4q5GJVEmB9QXxSt0sv4b5ufr3PEJR0DEZ9fKL038nVZyV6OEbSzolif04iFGPHycmRU4zErm5vJyN/FJTxptwkPWlgpyBx4ctFICqtBGAivRqCg3l59icEtOTo94Paz8cFidI1YTolj6hG2KbrZWGG4DGZ6O9CjcSkyYk3BJLxPGLPS8rW/zy1Fp3qXA4q1CCpYi4FQ7o1L8l8OOkBp0Uwq332ijO0pezOt4CyGqMdmITKQpQS2kE1nJpMi9A+Flu/T9ZrgrRgm8NVARlSYUc0QmnQObzMtwmmLhY3K6nesToQraikhnMqpHnmYhtuHH3JYGC7zeLR8RHZqqb7LCLZf4jmNDQbAtCHVgo5sQxw84dZKNKqWIbD1Jbkd9vOoNUM2I/TJQDtwVjzljlWdzIq68q3IKZBJS1x/n35vlyyNaEHSs3TdM0yYnKDukFqWbflzwm7Zl+X1NsuRSAdmShc4jzDGreUm9fRUTY+vtTjbIyo+Sv4nmVmWEjV9NH/DL93m9yKn10ZOOgEp/OFGE/nvapV2v+jz96plC0GCKUgJ/KxFqbxfW6T596bODsqr66553Q+ZjT/yBHzN7tO3T4SXRdn/qQvPkP+yAg76wtPwiFspQ== sam.crang@digital.cabinet-office.gov.uk GPG:B91590AEA3A6B81BCAE99ADC1F07315057BC3A55"]
     user_data_bucket_name = "${var.user_data_bucket_name}"
     user_data_bucket_region = "${var.user_data_bucket_region}"
     k8s_tag = "${var.k8s_tag}"
