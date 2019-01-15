9e073eb4fd756ee510943d55d29c6d9469031300
David Pye <david.pye@digital.cabinet-office.gov.uk>
2019-01-15 14:07:25 +0000

    include the other files in case required

---
+++ terraform/clusters/davidpye.run-sandbox.aws.ext.govsvc.uk/cluster.tf
@@ -0,0 +1,67 @@
+terraform {
+  backend "s3" {
+    bucket = "gds-re-run-sandbox-terraform-state"
+    region = "eu-west-2"
+    key    = "davidpye.run-sandbox.aws.ext.govsvc.uk/cluster.tfstate"
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+module "gsp-cluster" {
+    source = "git::https://github.com/alphagov/gsp-terraform-ignition//modules/gsp-cluster"
+    cluster_name = "davidpye"
+    dns_zone = "run-sandbox.aws.ext.govsvc.uk"
+    user_data_bucket_name = "gds-re-run-sandbox-terraform-state"
+    user_data_bucket_region = "eu-west-2"
+    k8s_tag = "v1.12.2"
+    admin_role_arns = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/admin"]
+
+    addons = {
+      ingress = 1
+      canary = 0
+      monitoring = 1
+      secrets = 1
+      ci = 1
+    }
+}
+
+module "main-pipelines" {
+  source = "git::https://github.com/alphagov/gsp-terraform-ignition//modules/flux-release"
+
+  namespace      = "${module.gsp-cluster.ci-system-release-name}-main"
+  chart_git      = "https://github.com/alphagov/gsp-ci-pipelines.git"
+  chart_ref      = "master"
+  chart_path     = "charts/pipelines"
+  cluster_name   = "${module.gsp-cluster.cluster-name}"
+  cluster_domain = "${module.gsp-cluster.cluster-domain-suffix}"
+  addons_dir     = "addons/${module.gsp-cluster.cluster-name}"
+  values = <<HEREDOC
+    ecr:
+      registry: ${data.aws_caller_identity.current.account_id}.dkr.ecr.eu-west-2.amazonaws.com
+      region: eu-west-2
+HEREDOC
+}
+
+module "prototype-kit" {
+  source = "git::https://github.com/alphagov/gsp-terraform-ignition//modules/flux-release"
+
+  namespace      = "gsp-prototype-kit"
+  chart_git      = "https://github.com/alphagov/gsp-govuk-prototype-kit.git"
+  chart_ref      = "gsp"
+  chart_path     = "charts/govuk-prototype-kit"
+  cluster_name   = "${module.gsp-cluster.cluster-name}"
+  cluster_domain = "${module.gsp-cluster.cluster-domain-suffix}"
+  addons_dir     = "addons/${module.gsp-cluster.cluster-name}"
+  values = <<EOF
+    ingress:
+      hosts:
+        - pk.${module.gsp-cluster.cluster-domain-suffix}
+        - prototype-kit.${module.gsp-cluster.cluster-domain-suffix}
+      tls:
+        - secretName: prototype-kit-tls
+          hosts:
+            - pk.${module.gsp-cluster.cluster-domain-suffix}
+            - prototype-kit.${module.gsp-cluster.cluster-domain-suffix}
+EOF
+}
