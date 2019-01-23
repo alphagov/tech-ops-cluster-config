2ca3a3609f57766fae4e59a9e1a22805aa2ce171
Sam Crang <sam.crang@gmail.com>
2019-01-23 16:12:09 +0000

    Add required configuration for eidas pipelines

--- terraform/clusters/samcrang.run-sandbox.aws.ext.govsvc.uk/cluster.tf
+++ terraform/clusters/samcrang.run-sandbox.aws.ext.govsvc.uk/cluster.tf
@@ -32,16 +32,23 @@ module "main-pipelines" {
   source = "git::https://github.com/alphagov/gsp-terraform-ignition//modules/flux-release"
 
   namespace      = "${module.gsp-cluster.ci-system-release-name}-main"
-  chart_git      = "https://github.com/alphagov/gsp-ci-pipelines.git"
-  chart_ref      = "master"
-  chart_path     = "charts/pipelines"
+  chart_git      = "https://github.com/alphagov/verify-eidas-pipelines.git"
+  chart_ref      = "develop"
+  chart_path     = "."
   cluster_name   = "${module.gsp-cluster.cluster-name}"
   cluster_domain = "${module.gsp-cluster.cluster-domain-suffix}"
   addons_dir     = "addons/${module.gsp-cluster.cluster-name}"
   values = <<HEREDOC
-    ecr:
-      registry: ${data.aws_caller_identity.current.account_id}.dkr.ecr.eu-west-2.amazonaws.com
-      region: eu-west-2
+    harbor:
+      keys:
+        ci: "${module.gsp-cluster.notary-ci-private-key}"
+        root: "${module.gsp-cluster.notary-root-private-key}"
+      passphrase:
+        delegation: "${module.gsp-cluster.notary-delegation-passphrase}"
+        root: "${module.gsp-cluster.notary-root-passphrase}"
+        snapshot: "${module.gsp-cluster.notary-snapshot-passphrase}"
+        targets: "${module.gsp-cluster.notary-targets-passphrase}"
+      password: "${module.gsp-cluster.harbor-password}"
 HEREDOC
 }
 
