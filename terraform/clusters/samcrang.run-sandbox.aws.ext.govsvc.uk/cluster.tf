edfd1f5f43b56055a390a135645987d8b64b89c8
Sam Crang <sam.crang@gmail.com>
2019-02-08 11:13:07 +0000

    WIP

--- terraform/clusters/samcrang.run-sandbox.aws.ext.govsvc.uk/cluster.tf
+++ terraform/clusters/samcrang.run-sandbox.aws.ext.govsvc.uk/cluster.tf
@@ -50,3 +50,27 @@ module "prototype-kit" {
             - prototype-kit.${module.gsp-cluster.cluster-domain-suffix}
 EOF
 }
+
+module "licensify-pipelines" {
+  source = "git::https://github.com/alphagov/gsp-terraform-ignition//modules/flux-release"
+
+  namespace      = "${module.gsp-cluster.ci-system-release-name}-main"
+  chart_git      = "https://github.com/alphagov/licensify-pipelines.git"
+  chart_ref      = "wip-pipelines"
+  chart_path     = "."
+  cluster_name   = "${module.gsp-cluster.cluster-name}"
+  cluster_domain = "${module.gsp-cluster.cluster-domain-suffix}"
+  addons_dir     = "addons/${module.gsp-cluster.cluster-name}"
+  values = <<HEREDOC
+    harbor:
+      keys:
+        ci: "${module.gsp-cluster.notary-ci-private-key}"
+        root: "${module.gsp-cluster.notary-root-private-key}"
+      passphrase:
+        delegation: "${module.gsp-cluster.notary-delegation-passphrase}"
+        root: "${module.gsp-cluster.notary-root-passphrase}"
+        snapshot: "${module.gsp-cluster.notary-snapshot-passphrase}"
+        targets: "${module.gsp-cluster.notary-targets-passphrase}"
+      password: "${module.gsp-cluster.harbor-password}"
+HEREDOC
+}
