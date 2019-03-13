f6ff1c7fe13497cfcd39a1f07fa690991ce78219
Daniel Blair <daniel.blair@digital.cabinet-office.gov.uk>
2019-03-13 15:45:31 +0000

    Add missing variables for terraforming hsm module in persistent state.
    Some variables were missed when migrating the hsm and other state out of
    the cluster and into a dedicated persistent terraform statefile. This
    updates the pipeline to pass the variables through to the hsm module.

--- terraform/accounts/verify/persistent/staging/resources.tf
+++ terraform/accounts/verify/persistent/staging/resources.tf
@@ -10,7 +10,11 @@ module "gsp-persistent" {
 }
 
 module "hsm" {
-  source       = "../../../../modules/hsm"
-  cluster_name = "${module.gsp-network.cluster-name}"
-  subnet_ids   = "${module.gsp-network.private_subnet_ids}"
+  source           = "../../../../modules/hsm"
+  cluster_name     = "${module.gsp-network.cluster-name}"
+  subnet_ids       = "${module.gsp-network.private_subnet_ids}"
+  subnet_count     = "${module.gsp-network.private-subnet-count}" // https://github.com/hashicorp/terraform/issues/12570
+  splunk_hec_url   = "${var.splunk_hec_url}"
+  splunk_hec_token = "${var.splunk_hec_token}"
+  splunk_index     = "verify_notification_hsm"
 }
