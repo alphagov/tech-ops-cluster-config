b195db92ab17d88421d5da56ad1f09e27c703697
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2019-04-10 11:30:26 +0100

    move supurfluous inputs/outputs

--- terraform/accounts/sandbox/common/variables.tf
+++ terraform/accounts/sandbox/common/variables.tf
@@ -8,13 +8,4 @@ variable "account_name" {
   type = "string"
 }
 
-variable "cluster_name" {
-  description = "Name of the cluster"
-  type        = "string"
-}
-
-variable "dns_zone" {
-  description = "The apex domain associated with this cluster"
-  type        = "string"
-}
 
