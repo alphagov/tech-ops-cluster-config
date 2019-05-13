8f938d269b8fe1989f6da3034f3a5364bcad5d9b
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2019-05-13 13:39:51 +0100

    prevent pushing stale pipeline
    adds some sanity checks to the set-pipeline script so that we don't
    accidently push up an out of sync version of the deployer pipeline
    
    this is only required as we have not got self-updating pipeline yet

--- hack/set-pipeline.sh
+++ hack/set-pipeline.sh
@@ -2,11 +2,34 @@
 
 set -eu
 
+PLATFORM_SRC="../gsp-terraform-ignition"
+
 : "${CLUSTER_NAME:?}"
+
+function git-status-not-say {
+  ( \
+    cd "${PLATFORM_SRC}" \
+    && git fetch --all \
+    && git status --ignore-submodules | grep -i -v "${1}" \
+  )
+}
+
+if [[ "$(cd ${PLATFORM_SRC} && git rev-parse --abbrev-ref HEAD)" != "master" ]]; then
+  echo "aborting: working copy of ${PLATFORM_SRC} is not on master"
+  exit 1
+fi
+if git-status-not-say "working tree clean"; then
+  echo "aborting: working copy of ${PLATFORM_SRC} is not clean"
+  exit 1
+fi
+if git-status-not-say "your branch is up to date"; then
+  echo "aborting: working copy of ${PLATFORM_SRC} is not up to date with the remote repository"
+  exit 1
+fi
 
 fly -t cd-gsp sync
 
-fly -t cd-gsp set-pipeline -p "${CLUSTER_NAME}-deployer" --config ../gsp-terraform-ignition/pipelines/deployer/deployer.yaml \
+fly -t cd-gsp set-pipeline -p "${CLUSTER_NAME}-deployer" --config "${PLATFORM_SRC}/pipelines/deployer/deployer.yaml" \
 	--load-vars-from ./clusters/${CLUSTER_NAME}.yaml \
 	--check-creds
 
